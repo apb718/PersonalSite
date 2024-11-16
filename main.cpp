a2129b993f7dd519608690bed48ecd29f2048157
apb718 <apb718@gmail.com>
2024-11-16 13:38:52 -0500

    add files for docker startup

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        cout << "Content-Type: text/html\r\n\r\n";
+        cout << "<html><head><title>Hello World</title></head>";
+        cout << "<body>"
+             << "<h1>Hello Chat GPT!</h1>"
+             << "</body></html>";
+}
