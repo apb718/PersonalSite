d26ce9fbfdeac83e0aca5a9f438fd7b802ba7757
apb718 <apb718@gmail.com>
2025-02-17 17:07:46 -0500

    adjust for merge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import Navbar from '$lib/Components/Navbar.svelte';
-	import Footer from '$lib/Components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import 'iconify-icon';
-	import Banner from '$lib/Components/Banner.svelte';
+	import Banner from '$lib/components/Banner.svelte';
 	import BannerImg from '$lib/assets/images/mountain-top.jpeg';
 	let { children } = $props();
 </script>
 
-<<<<<<< HEAD
 <Navbar />
-<!--<div id="big" class="h-10000" style="height: 1000vh">e</div>-->
 <Banner imageUrl={BannerImg} alt="Welcome Banner" text="Welcome" />
-=======
-
-
-<Navbar />
-<Banner imageUrl={BannerImg} alt="Welcome Banner" text="Welcome"/>
->>>>>>> origin/main
 
 {@render children()}
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-<<<<<<< HEAD
 
 export default defineConfig({
 	plugins: [sveltekit()]
 });
-=======
-import tailwindcss from '@tailwindcss/vite';
-
-
-export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-	],
-});
->>>>>>> origin/main
