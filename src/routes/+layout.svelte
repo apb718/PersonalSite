c8f55f5092e3b675f841e541657e6ac68ffcf616
apb718 <apb718@gmail.com>
2025-02-17 16:03:58 -0500

    migrate to correct sv

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/Components/Navbar.svelte';
+	import Footer from '$lib/Components/Footer.svelte';
+	import 'iconify-icon';
+	import Banner from '$lib/Components/Banner.svelte';
+	import BannerImg from '$lib/assets/images/mountain-top.jpeg';
+	let { children } = $props();
+</script>
+
+<Navbar />
+<Banner imageUrl={BannerImg} alt="Welcome Banner" text="Welcome" />
+
+{@render children()}
+
+<Footer />
