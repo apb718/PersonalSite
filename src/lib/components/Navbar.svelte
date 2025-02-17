a9a84835ee3f3ee93f27accddd92fb573859f4c4
apb718 <apb718@gmail.com>
2025-02-17 17:12:26 -0500

    Fixed invalid href attributes in Navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,14 +4,14 @@
 
 <nav class="bg-apb-b flex items-center justify-between px-32 py-4 ">
 	<!-- Logo -->
-	<a href="">
+	<a href="#banner" aria-label="Top">
 		<img class="h-16 w-auto rounded-md" src="{Logo}" alt="AB Logo"/>
 	</a>
 	<!-- Navigation Links -->
 	<ul class="flex space-x-6">
-		<li><a href="#portfolio" class="text-apb-y text-lg font-semibold hover:underline">Portfolio</a></li>
-		<li><a href="" class="text-apb-y text-lg font-semibold hover:underline">About</a></li>
-		<li><a href="" class="text-apb-y text-lg font-semibold hover:underline">Interests</a></li>
-		<li><a href="" class="text-apb-y text-lg font-semibold hover:underline">Contact</a></li>
+		<li><a aria-label="Portfolio" href="#portfolio" class="text-apb-y text-lg font-semibold hover:underline">Portfolio</a></li>
+		<li><a aria-label="About" href="#about" class="text-apb-y text-lg font-semibold hover:underline">About</a></li>
+		<li><a aria-label="Interests" href="#interests" class="text-apb-y text-lg font-semibold hover:underline">Interests</a></li>
+		<li><a aria-label="Contact" href="#contact" class="text-apb-y text-lg font-semibold hover:underline">Contact</a></li>
 	</ul>
 </nav>
