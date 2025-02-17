c774bac2ebf68f8fb19183ec28e9645c007a7027
apb718 <apb718@gmail.com>
2025-02-17 17:16:22 -0500

    changes to npm

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -34,8 +34,8 @@ declare module '$env/static/private' {
 	export const INIT_CWD: string;
 	export const SHELL: string;
 	export const TERM: string;
-	export const TMPDIR: string;
 	export const HOMEBREW_REPOSITORY: string;
+	export const TMPDIR: string;
 	export const npm_config_global_prefix: string;
 	export const TERM_SESSION_ID: string;
 	export const COLOR: string;
@@ -124,8 +124,8 @@ declare module '$env/dynamic/private' {
 		INIT_CWD: string;
 		SHELL: string;
 		TERM: string;
-		TMPDIR: string;
 		HOMEBREW_REPOSITORY: string;
+		TMPDIR: string;
 		npm_config_global_prefix: string;
 		TERM_SESSION_ID: string;
 		COLOR: string;
