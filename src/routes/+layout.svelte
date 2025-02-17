4c0edfaadcf0344182ea93cd36a82d8471401b4e
apb718 <apb718@gmail.com>
2025-02-17 17:42:57 -0500

    manage merge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import Navbar from '$lib/Components/Navbar.svelte';
-	import Footer from '$lib/Components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import 'iconify-icon';
-	import Banner from '$lib/Components/Banner.svelte';
+	import Banner from '$lib/components/Banner.svelte';
 	import BannerImg from '$lib/assets/images/mountain-top.jpeg';
 	let { children } = $props();
 </script>
 
-
-
 <Navbar />
-<Banner imageUrl={BannerImg} alt="Welcome Banner" text="Welcome"/>
+<Banner imageUrl={BannerImg} alt="Welcome Banner" text="Welcome" />
 
 {@render children()}
 
