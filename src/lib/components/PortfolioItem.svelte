e5afa71d0663953e85a1de05f0d78d9e536114d4
apb718 <apb718@gmail.com>
2025-02-17 17:50:53 -0500

    navbar

--- src/lib/components/PortfolioItem.svelte
+++ src/lib/components/PortfolioItem.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <!-- Compute grid span based on size -->
-<div class={`flex items-center justify-center text-2xl font-bold h-200 ${color} ${className}
+<div class={`flex items-center justify-center text-center text-2xl font-bold  ${color} ${className}
 		${size === "2x2" ? "col-span-2 row-span-2" : ""}
 		${size === "1x2" ? "row-span-2" : ""}
 		${size === "2x1" ? "col-span-2" : ""}
