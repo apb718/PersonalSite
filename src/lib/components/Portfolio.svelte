d13078522d18d9f7a59c6d2e9ef8edfbb84fdf73
Alec Byrd <apb718@gmail.com>
2025-03-09 23:03:35 -0400

    resolve issue with images not rendering on main page

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -13,7 +13,7 @@
 				title: "Chicago Raw Food",
 				description: "A website for a Chicago-based raw food company. Features include online ordering, nutritional information, and a backend to support order management and changes.",
 				technologies: ["Sveltekit", "Full-Stack Development", "Shadcn", "HTML/CSS/JS/TS", "Vercel", "tailwind", "Stripe", "MySQL"],
-				imageUrl: "src/lib/assets/images/raw.webp",
+				imageUrl: "/images/raw.webp",
 				link: "https://chicago-raw-food.vercel.app/",
 			}
 		},
@@ -26,7 +26,7 @@
 				title: "CME Group",
 				description: "Worked on a team of field engineers who manage and maintain servers for CME Group's high-frequency trading servers.  Companies included are 3Red, Jump, Akuna Capital, and international clients.",
 				technologies: ["Hardware Troubleshooting", "HFT Server Management", "Linux CLI", "Customer Service", "Networking"],
-				imageUrl: "src/lib/assets/images/CME.webp",
+				imageUrl: "/images/CME.webp",
 				// link: "#",
 			}
 		},
@@ -39,7 +39,7 @@
 				title: "Theta Tau Website",
 				description: "Redesigned and developed the website for Theta Tau, the professional engineering fraternity at my university. Implemented CI/CD pipelines for preview, and focused on modifying to make site more maintainable. This primarly regarding rebuilding towrads a component architecture.",
 				technologies: ["HTML/CSS/JS", "Firebase", "Github Actions", "Code Quality Assurance"],
-				imageUrl: "src/lib/assets/images/thetatau.webp",
+				imageUrl: "/images/thetatau.webp",
 				link: "#",
 			}
 		},
@@ -53,7 +53,7 @@
 				description: "A collection of projects developed during my 3 years at Miami University.  Includes projects in C++, Python, and Java often revolving around creating maintainable code that is easy to read and understand.  Core ideas include OOP, data structures, algorithms, and data representation.",
 				technologies: ["Java", "Python", "C++", "OOP", "Scikit-learn", "HPC", "Distributed Computing", "Networking", "Troubleshooting", "SDLC", "Docker", "DLV/Prolog" ],
 				link: "#",
-				imageUrl: "src/lib/assets/images/miami.webp",
+				imageUrl: "/images/miami.webp",
 			}
 		}
 	];
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,13 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import 'iconify-icon';
 	import Banner from '$lib/components/Banner.svelte';
-	import BannerImg from '$lib/assets/images/mountain-top.jpeg';
+
 	let { children } = $props();
 </script>
 
 <Navbar />
 <!--<div id="big" class="h-10000" style="height: 1000vh">e</div>-->
-<Banner imageUrl={BannerImg} alt="Welcome Banner" text="Welcome" />
+<Banner imageUrl="images/mountain-top.webp" alt="Welcome Banner" text="Welcome" />
 
 {@render children()}
 
